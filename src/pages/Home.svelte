9adc23e30c00bbbe57a5aa4dcc162897fe2ba8c5
Lincxx <bossco28@gmail.com>
2019-06-08 09:45:42 -0400

    init commit

---
+++ src/pages/Home.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { onMount } from "svelte";
+  import Postform from "../components/Postform.svelte";
+
+  const apiBase = "https://ndb99xkpdk.execute-api.eu-west-2.amazonaws.com/dev";
+  let posts = [];
+
+  onMount(async () => {
+    const res = await fetch(apiBase + "/posts");
+    posts = await res.json();
+  });
+
+  function editPost(post) {
+    console.log(post);
+  }
+
+  function deletePost(id) {
+    console.log(`Deleting post with id ${id}`);
+  }
+
+  function addPost({ detail: post }) {
+    posts = [post, ...posts];
+  }
+</script>
+
+<style>
+  .delete-btn {
+    color: red !important;
+  }
+
+  .card .card-content .card-title {
+    margin-bottom: 0;
+  }
+
+  .card .card-content p.timestamp {
+    color: #999;
+    margin-bottom: 10px;
+  }
+</style>
+
+<div class="row">
+  <div class="col s6">
+    <Postform on:postCreated={addPost} />
+  </div>
+</div>
+
+<div class="row">
+  {#if posts.length === 0}
+    <h3>Loading post....</h3>
+  {:else}
+    {#each posts as post}
+      <div class="col s6">
+        <div class="card">
+          <div class="card-content">
+            <p class="card-title">{post.title}</p>
+            <p class="timestamp">{post.createdAt}</p>
+            <p>{post.body}</p>
+          </div>
+          <div class="card-action">
+            <a href="#" on:click={() => editPost(post)}>Edit</a>
+            <a href="#" class="delete-btn" on:click={() => deletePost(post.id)}>
+              Delete
+            </a>
+          </div>
+        </div>
+      </div>
+    {/each}
+  {/if}
+</div>
